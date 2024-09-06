954af704f4c84c1a1f493ecd75801d957a03ddf9
andrew540714 <andrews117007@gmail.com>
2024-09-06 09:08:10 -0700

    add css

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+ $(document).ready(function (){
+ 
+	$(".correct").click(function() {
+		$(this).css("color", "green");
+	});
+	$(".incorrect").click(function() {
+		$(this).css("color", "red");
+	});
+ });	
